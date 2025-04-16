816809dae631e7423cce50e8134b013b39c21681
Gabriella Dines <124473160+gabrielladines@users.noreply.github.com>
2025-04-16 17:11:05 +0100

    Add files via upload

---
+++ week_8.py
@@ -0,0 +1,13 @@
+# Week 8: Hanoi Rings.
+
+def hanoi(n,start,end,aux):
+    if n == 1:
+        print("Move ring 1 from " + start + " to " + end + ".")
+    else:
+        hanoi(n-1, start, aux, end)
+        print("Move ring " + str(n) + " from " + start + " to " + end + ".")
+        hanoi(n-1, aux, end, start)
+
+n = 6 
+hanoi(n, "a", "c", "b")
+        
